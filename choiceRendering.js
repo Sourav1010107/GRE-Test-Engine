4bc69c8c54929142d68dc146b3f245d62741700e
Sourav <sourav1010107@gmail.com>
2026-08-30 13:11:02 +0600

    minor

--- choiceRendering.js
+++ choiceRendering.js
@@ -191,5 +191,53 @@ function renderReadingPassage(){
 
 }
 
+//--------------------------------------
+//        QUANTATIVE QUESTION
+//--------------------------------------
 
+function renderquantQuestion(){
+
+    let container = document.querySelector('#choices');
+    let containerPassage = document.querySelector('#passage');
+    container.innerHTML= "";
+    containerPassage.innerHTML = "";
+
+    questions[currentQuestion].choices.forEach((choice)=>{
+        const checkbox = document.createElement('input');
+        const label = document.createElement('label');
+        const linebreak = document.createElement('br');
+
+        checkbox.type= "checkbox";
+        checkbox.value= choice;
+
+        //creating the checkbox element prop.
+        label.append(checkbox);
+        label.append(' '+ choice);
+
+        //loading the saved answers in the question paper
+        checkbox.checked = answers[currentQuestion].includes(choice);
+
+        //save the choice in the answer array
+        checkbox.addEventListener("change", ()=>{
+
+            if(answers[currentQuestion].length >= 2){
+                checkbox.checked = false;
+            }
+            if (checkbox.checked) {
+                if (!answers[currentQuestion].includes(choice)) {
+                    answers[currentQuestion].push(choice);
+                }
+            }
+            else{
+                answers[currentQuestion] = answers[currentQuestion].filter(
+                    savedChoice => savedChoice != choice
+                );
+            }              
+        });
     
+        //add choices in the HTML element
+        container.append(label);
+        container.append(linebreak);
+
+    });
+}
